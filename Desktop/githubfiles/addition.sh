40069c22991f4b330d0276b9018fb3376870d5fd
priyanka897 <priyankamore8997@gmail.com>
2021-08-17 13:13:34 +0530

    commit messege

---
+++ Desktop/githubfiles/addition.sh
@@ -0,0 +1,14 @@
+echo "Enter First Number"
+read a
+echo "Enter Second Number"
+read b
+c=$(( $a+$b ))
+d=$(( $a-$b ))
+e=$(( $a*$b ))
+f=$(( $a/$b ))
+echo "Addition :"$c
+echo "subtraction:"$d
+echo "Multiplication:"$e
+echo "Division:"$f
+
+
