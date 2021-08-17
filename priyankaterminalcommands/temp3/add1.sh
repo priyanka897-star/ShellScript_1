6232a6cd5b1734a58af12e76e25879fbc945c414
priyanka897 <priyankamore8997@gmail.com>
2021-08-17 12:38:27 +0530

    modify add1.sh

--- priyankaterminalcommands/temp3/add1.sh
+++ priyankaterminalcommands/temp3/add1.sh
@@ -3,4 +3,11 @@ read a
 echo "enter the number2"
 read b
 c=(( $a+$b ))
+d=(( $a-$b ))
+
+e=(( $a*$b ))
+f=(( $a\$b ))
 echo "Addition:" $c
+echo "Subtraction:" $d
+echo "Multiplication:" $e
+echo "Division:" $f
